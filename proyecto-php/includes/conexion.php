1f7de1b40779dce4388f9fd516fb1e9f1a9e61ae
CuartoHokage <jaime199505@hotmail.com>
2018-12-25 17:35:16 -0500

    Arreglando el cotejamiento de la conexión a la base de datos

--- proyecto-php/includes/conexion.php
+++ proyecto-php/includes/conexion.php
@@ -5,7 +5,7 @@ $password='';
 $database='blog_master';
 $db=mysqli_connect($server, $username, $password, $database);
 
-mysqli_query($db, "SET NAMES 'utf8_general_ci'");
+mysqli_query($db, "SET NAMES 'utf8'");
 
 // Iniciar la sesion
 session_start();
